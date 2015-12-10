d3b72c81a4ad9dc0c355f604effd7be9db4d4eed
SamWang1995 <Sam@host-46-84.aztevds.tempe.az.us.clients.pavlovmedia.com>
2015-12-09 22:58:44 -0700

    Init Commit

---
+++ Solo2Duo/Solo2Duo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Solo2Duo
+//
+//  Created by Samuel Wang on 12/9/15.
+//  Copyright © 2015 Samuel Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
