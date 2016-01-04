1071d5658a8877f819969a1d447a4cb0113ce4b3
Betty Jing <bettyjing@Bettys-MacBook-Air.local>
2016-01-04 12:08:18 -0800

    attempt at grabbing profile picture. doesn't work but doesn't crash.

--- Solo2Duo/Solo2Duo/profile.swift
+++ Solo2Duo/Solo2Duo/profile.swift
@@ -7,9 +7,42 @@
 //
 
 import Foundation
+import Parse
+import FBSDKCoreKit
+import FBSDKLoginKit
+
 
 class profile : UIViewController {
+ 
+    @IBOutlet var profilePicture: UIImageView!
     override func viewDidLoad() {
+        
+        super.viewDidLoad()
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer()) //let's you go back with gesture
+        
+        if let currentUser = PFUser.currentUser() {
+            
+            let accessToken = FBSDKAccessToken.currentAccessToken()
+            let userID = FBSDKAccessToken.currentAccessToken().userID
+            let url = NSURL(string: "https://graph.facebook.com/"+userID+"/picture?width=300&height=300")
+            let urlRequest = NSURLRequest(URL: url!)
+            
+            NSURLConnection.sendAsynchronousRequest(urlRequest, queue: NSOperationQueue.mainQueue(), completionHandler: {
+                response, data, error in
+                
+                let image = UIImage(data: data!)
+                self.profilePicture.image = image
+                
+                currentUser.saveInBackground()
+                FBSDKProfile.enableUpdatesOnAccessTokenChange(true)
+                
+            })
+           
+            
+            
+            
+        }
+        
     }
+    
 }
